f0cbdcf0167ed3f141c90544b96f033356b14357
Fernando Castro <devfercastro@gmail.com>
2024-11-21 19:54:27 -0300

    feat: improve general pomodoro logic
    - Add modes types and constants
    - Improve app global state management
    - Add mode transition logic between work, shortBreak and longBreak
    - Add pomodoros completed counter
    - Document by adding explanatory coments

--- App.tsx
+++ App.tsx
@@ -1,96 +1,163 @@
 import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
 import { useState, useEffect } from "react";
-import { formatTime, formatProgress } from "./helpers";
 import Timer from "./components/Timer";
 
-const BORDER = 20;
-const POMODORO_TIME = 1 * 60;
+type Mode = {
+  name: "work" | "shortBreak" | "longBreak";
+  label: string;
+  duration: number;
+};
 
-type appStatus = {
+const MODES: { WORK: Mode; SHORT_BREAK: Mode; LONG_BREAK: Mode } = {
+  WORK: {
+    name: "work",
+    label: "Work",
+    duration: 25,
+  },
+  SHORT_BREAK: {
+    name: "shortBreak",
+    label: "Short Break",
+    duration: 5,
+  },
+  LONG_BREAK: {
+    name: "longBreak",
+    label: "Long Break",
+    duration: 15,
+  },
+};
+
+type AppState = {
   isRunning: boolean;
-  mode: "work" | "break";
+  autoStart: boolean;
+  pomodorosCompleted: number;
+  timeLeft: number;
+  mode: Mode;
+};
+
+const DEFAULT_STATE: AppState = {
+  isRunning: false,
+  autoStart: false,
+  pomodorosCompleted: 0,
+  timeLeft: MODES.WORK.duration,
+  mode: MODES.WORK,
 };
 
 export default function App() {
-  const [timeLeft, setTimeLeft] = useState(POMODORO_TIME);
-  const [appStatus, setAppStatus] = useState<appStatus>({
-    isRunning: false,
-    mode: "work",
-  });
+  const [appState, setAppState] = useState<AppState>(DEFAULT_STATE);
 
   useEffect(() => {
     let interval: NodeJS.Timeout;
 
-    if (appStatus.isRunning && timeLeft > 0) {
+    // If the timer is running and timeLeft > 0, decrement the timeLeft
+    if (appState.isRunning && appState.timeLeft > 0) {
       interval = setInterval(() => {
-        setTimeLeft((prevTime) => {
-          if (prevTime <= 1) {
-            setAppStatus((prevStatus) => {
-              return {
-                ...prevStatus,
-                isRunning: false,
-              };
-            });
-            clearInterval(interval);
-            return 0;
-          }
-          return prevTime - 1;
-        });
+        setAppState((prevState) => ({
+          ...prevState,
+          timeLeft: prevState.timeLeft - 1,
+        }));
       }, 100);
+    }
+    // If the timer is finished, change the mode
+    else if (appState.timeLeft === 0) {
+      handleModeTransition();
     }
 
     return () => clearInterval(interval);
-  }, [appStatus.isRunning, timeLeft]);
+  }, [appState.isRunning, appState.timeLeft]);
 
-  const startHandler = () => {
-    setAppStatus((prevStatus) => {
-      return {
-        ...prevStatus,
-        isRunning: !prevStatus.isRunning,
-      };
-    });
+  /**
+   * Handles the mode transitions
+   */
+  const handleModeTransition = () => {
+    // If autoStart is disabled, stop the timer
+    if (!appState.autoStart) {
+      setAppState((prevState) => ({
+        ...prevState,
+        isRunning: false,
+      }));
+    }
+    // On work mode
+    if (appState.mode.name === "work") {
+      // Increment the completed pomodoros counter
+      setAppState((prevState) => ({
+        ...prevState,
+        pomodorosCompleted: prevState.pomodorosCompleted + 1,
+      }));
+      // If pomodorosCompleted < 4, switch to short break
+      if (appState.pomodorosCompleted < 4) {
+        setAppState((prevState) => ({
+          ...prevState,
+          mode: MODES.SHORT_BREAK,
+          timeLeft: MODES.SHORT_BREAK.duration,
+        }));
+      }
+      // If pomodorosCompleted >= 4, switch to long break
+      else {
+        setAppState((prevState) => ({
+          ...prevState,
+          mode: MODES.LONG_BREAK,
+          timeLeft: MODES.LONG_BREAK.duration,
+          pomodorosCompleted: 0,
+        }));
+      }
+    }
+    // On short break or long break mode, switch to work mode
+    else if (
+      appState.mode.name === "shortBreak" ||
+      appState.mode.name === "longBreak"
+    ) {
+      setAppState((prevState) => ({
+        ...prevState,
+        mode: MODES.WORK,
+        timeLeft: MODES.WORK.duration,
+      }));
+    }
   };
 
-  const resetHandler = () => {
-    setTimeLeft(POMODORO_TIME);
-    setAppStatus((prevStatus) => {
-      return {
-        ...prevStatus,
-        isRunning: false,
-      };
-    });
+  const handleStartPause = () => {
+    setAppState((prevState) => ({
+      ...prevState,
+      isRunning: !prevState.isRunning,
+    }));
+  };
+
+  const handleReset = () => {
+    setAppState((prevState) => DEFAULT_STATE);
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Pomodoro Tracker</Text>
+      {/* TODO: Refactor Timer component to rename props name */}
       <Timer
-        currentTime={timeLeft}
-        totalTime={POMODORO_TIME}
-        isRunning={appStatus.isRunning}
+        currentTime={appState.timeLeft}
+        totalTime={appState.mode.duration}
+        isRunning={appState.isRunning}
       />
       <View style={styles.controlsContainer}>
-        <TouchableOpacity style={styles.button} onPress={startHandler}>
+        <TouchableOpacity style={styles.button} onPress={handleStartPause}>
           <Text style={styles.buttonText}>
-            {appStatus.isRunning ? "Pause" : "Start"}
+            {appState.isRunning ? "Pause" : "Start"}
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={resetHandler}>
+        <TouchableOpacity style={styles.button} onPress={handleReset}>
           <Text style={styles.buttonText}>Reset</Text>
         </TouchableOpacity>
       </View>
 
+      {/* TODO: Refactor, move to a dedicated components. Eg. Metrics.tsx */}
       <View style={styles.modeContainer}>
         <Text style={styles.mode}>Mode: </Text>
         <Text
           style={[
             styles.mode,
-            appStatus.mode === "work" ? styles.modeWork : styles.modeBreak,
+            appState.mode.name === "work" ? styles.modeWork : styles.modeBreak,
           ]}
         >
-          {appStatus.mode}
+          {appState.mode.label}
         </Text>
       </View>
+      <Text>Pomodoros completed: {appState.pomodorosCompleted}</Text>
     </View>
   );
 }
