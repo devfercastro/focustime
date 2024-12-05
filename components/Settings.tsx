b1ff1917b52c566aea220d156f067489802ae74b
Fernando Castro <devfercastro@gmail.com>
2024-12-05 20:20:21 -0300

    docs(Settings): add some comments

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
-import { StyleSheet, Text, Animated, Dimensions } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { StyleSheet, Animated, Dimensions } from "react-native";
 import { NumberInput, SwitchInput } from "./Inputs";
 
 import { usePreferencesContext } from "../context/PreferencesContext";
 
+// get screen dimensions to do some stuff
 const [screenHeight, screenWidth] = [
   Dimensions.get("window").height,
   Dimensions.get("window").width,
@@ -13,27 +14,35 @@ export default function Settings() {
   const { preferences, setPreferences, isSettingsOpen } =
     usePreferencesContext();
 
+  // animated value for horizontal sliding animation
   const translateX = useRef(new Animated.Value(-screenWidth)).current;
 
+  // hook that trigger horizontal sliding animation when isSettingsOpen changes
+  // translateX was added cause biome cries to much if not
   useEffect(() => {
     Animated.timing(translateX, {
-      toValue: isSettingsOpen ? 0 : -screenWidth,
+      toValue: isSettingsOpen ? 0 : -screenWidth, // if settings are open, slide to left, otherwise slide to right
       duration: 200,
-      useNativeDriver: true,
+      useNativeDriver: true, // required flag, for some perfomance reasons (why not include it by default?)
     }).start();
   }, [isSettingsOpen, translateX]);
 
+  /*
+   * This function modifies modes duration (Work/Short Break/Long Break)
+   * @param the mode to change
+   * @param the number of seconds to add/subtract (depends if it's positive or negative)
+   */
   const handleModePreferencesChange = (
     modName: keyof Modes,
     number: number,
   ) => {
     // TODO: check if number is valid
     setPreferences({
-      ...preferences,
+      ...preferences, // left everything untouched but the modes object
       modes: {
-        ...preferences.modes,
+        ...preferences.modes, // left everything untouched but the mode object to change
         [modName]: {
-          ...preferences.modes[modName],
+          ...preferences.modes[modName], // left everything untouched but the duration property
           duration: preferences.modes[modName].duration + number,
         },
       },
@@ -45,23 +54,26 @@ export default function Settings() {
       <NumberInput
         label="Work"
         value={preferences.modes.WORK.duration}
-        increment={() => handleModePreferencesChange("WORK", 60)}
+        increment={() => handleModePreferencesChange("WORK", 60)} // !! add and subtract 1 minute in seconds, don't forget time is expressed in seconds on the whole app you idiot !!
         decrement={() => handleModePreferencesChange("WORK", -60)}
       />
+
       <NumberInput
         label="Short break"
         value={preferences.modes.SHORT_BREAK.duration}
         increment={() => handleModePreferencesChange("SHORT_BREAK", 60)}
         decrement={() => handleModePreferencesChange("SHORT_BREAK", -60)}
       />
+
       <NumberInput
         label="Long break"
         value={preferences.modes.LONG_BREAK.duration}
         increment={() => handleModePreferencesChange("LONG_BREAK", 60)}
         decrement={() => handleModePreferencesChange("LONG_BREAK", -60)}
       />
+
       <NumberInput
-        label="Pomodoros until long break:"
+        label="Pomodoros until long break"
         value={preferences.pomodorosUntilLongBreak}
         formatToMinutes={false}
         increment={() =>
@@ -77,6 +89,7 @@ export default function Settings() {
           })
         }
       />
+
       <SwitchInput
         label="Autostart next mode:"
         isChecked={preferences.autoStartNextMode}
@@ -94,7 +107,7 @@ export default function Settings() {
 const styles = StyleSheet.create({
   container: {
     width: screenWidth * 0.75, // take the 3/4 of the screen (TODO: change it to laptop)
-    height: screenHeight + 40, // for some reason I have to add 40px
+    height: screenHeight + 40, // for some reason I have to add 40px, I forgot why tbh
     backgroundColor: "#f2f2f2",
     position: "absolute",
     zIndex: 5, // SettingsBtn has zIndex of 10, and I don't want to cover it
